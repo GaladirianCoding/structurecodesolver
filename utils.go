a4da093bf5c760f4b0ec4a077f0f12405eb7167d
Luke C. J. Currie <lukecjcurrie@gmail.com>
2019-05-25 18:12:22 -0400

    fixed comments for util functions

--- utils.go
+++ utils.go
@@ -5,12 +5,12 @@ import (
 	"time"
 )
 
-//Remove returns a slice with puzzleRune at index i removed
+//Remove returns a puzzle with puzzleRune at index i removed
 func remove(slice puzzle, s int) puzzle {
 	return append(slice[:s], slice[s+1:]...)
 }
 
-//Shuffle shuffles a puzzleRune slice in place.
+//Shuffle shuffles a puzzle in place.
 func shuffle(vals puzzle) {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for n := len(vals); n > 0; n-- {
